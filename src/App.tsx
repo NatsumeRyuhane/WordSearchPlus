32eaf3241a163df988640a32ed953e387532c79c
kevinzhou <kevinzhou716@gmail.com>
2024-11-16 23:11:43 -0800

    Add word placement logic and create wordsearch ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import GameBoardComponent from "./components/GameBoardComponents";
 
 function App() {
   return (
     <>
+      <GameBoardComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
